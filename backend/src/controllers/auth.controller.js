446940c53f2890cda096685df534b98bcda6043e
Baber Khan <bbrkhnn@outlook.com>
2025-06-12 17:13:41 +0100

    feat(auth): complete user registration logic with validation, hashing, and test coverage

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,4 +1,58 @@
-export const registerUser = () => {};
+import bcrypt from "bcryptjs";
+
+import { AppError } from "../utils/AppError.js";
+import User from "../models/user.model.js";
+
+export const registerUser = async (req, res) => {
+  const { fullName, email, password } = req.body;
+
+  if (!fullName) throw new AppError("Full name is required", 400);
+
+  if (!email) throw new AppError("Email is required", 400);
+
+  if (!password) throw new AppError("Password is required", 400);
+
+  const trimmedFullName = fullName.trim();
+  const trimmedEmail = email.trim();
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+  if (!emailRegex.test(trimmedEmail)) throw new AppError("Invalid email", 400);
+
+  if (password.length < 8)
+    throw new AppError("Password should be at least 8 characters long", 400);
+
+  if (trimmedFullName.length < 3)
+    throw new AppError("Full name should be at least 3 characters long", 400);
+
+  if (trimmedFullName.length > 50)
+    throw new AppError("Full name should be less than 50 characters long", 400);
+
+  const existingUser = await User.findOne({ email: trimmedEmail });
+
+  if (existingUser) throw new AppError("Email already exists", 409);
+
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  const newUser = new User({
+    fullName: trimmedFullName,
+    email: trimmedEmail,
+    password: hashedPassword,
+  });
+
+  await newUser.save();
+
+  return res.status(201).json({
+    message: "User registered successfully",
+    user: {
+      _id: newUser._id,
+      fullName: newUser.fullName,
+      email: newUser.email,
+      profilePic: newUser.profilePic,
+      isVerified: newUser.isVerified,
+    },
+  });
+};
 
 export const loginUser = () => {};
 
