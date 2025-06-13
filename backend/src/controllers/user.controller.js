ceac41767f2d2584499562c8567ae1d7844b19bf
Baber Khan <bbrkhnn@outlook.com>
2025-06-13 02:08:58 +0100

    feat(user): implement getAllUsers() to return all users excluding the requester

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,4 +1,14 @@
-export const getAllUsers = () => {};
+import User from "../models/user.model";
+
+export const getAllUsers = async (req, res) => {
+  const currentUserId = req.user._id;
+
+  const users = await User.find({ _id: { $ne: currentUserId } })
+    .select("fullName email profilePic isVerified")
+    .lean();
+
+  res.status(200).json({ users });
+};
 
 export const getUserById = () => {};
 
