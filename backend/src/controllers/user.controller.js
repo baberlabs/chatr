d306673946213c705a5ac412ebd7954732951479
Baber Khan <bbrkhnn@outlook.com>
2025-06-13 12:27:52 +0100

    feat(user): implement getUserById() controller with validation and clean error handling

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,4 +1,6 @@
+import mongoose from "mongoose";
 import User from "../models/user.model.js";
+import { AppError } from "../utils/AppError.js";
 
 export const getAllUsers = async (req, res) => {
   const currentUserId = req.user._id;
@@ -10,7 +12,21 @@ export const getAllUsers = async (req, res) => {
   res.status(200).json({ users });
 };
 
-export const getUserById = () => {};
+export const getUserById = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    throw new AppError("Invalid User ID", 400);
+  }
+
+  const user = await User.findById(id).select("-password");
+
+  if (!user) {
+    throw new AppError("User Not Found", 404);
+  }
+
+  res.status(200).json({ user });
+};
 
 export const updateUserProfile = () => {};
 
