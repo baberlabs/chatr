00ac76eae34fdb6527d9c0b0da334284547de4e3
Baber Khan <bbrkhnn@outlook.com>
2025-06-15 11:38:41 +0100

    feat(chat): create chat model

---
+++ backend/src/models/chat.model.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+const chatSchema = new mongoose.Schema(
+  {
+    isGroup: {
+      type: Boolean,
+      default: false,
+    },
+    participants: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+      },
+    ],
+    chatName: {
+      type: String,
+      trim: true,
+    },
+    groupAdmin: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    latestMessage: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Message",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Chat = mongoose.model("Chat", chatSchema);
+
+export default Chat;
