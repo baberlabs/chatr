d2e4b81d5187eced6d3ce5ac27602da79bd4c798
Baber Khan <bbrkhnn@outlook.com>
2025-06-16 10:21:25 +0100

    feat(util): fix the conditional statement

--- backend/src/utils/cloudinary.js
+++ backend/src/utils/cloudinary.js
@@ -1,9 +1,9 @@
 import { v2 as cloudinary } from "cloudinary";
 
 if (
-  !process.env.CLOUDINARY_CLOUD_NAME ||
-  !process.env.CLOUDINARY_API_KEY ||
-  !process.env.CLOUDINARY_API_SECRET
+  process.env.CLOUDINARY_CLOUD_NAME &&
+  process.env.CLOUDINARY_API_KEY &&
+  process.env.CLOUDINARY_API_SECRET
 ) {
   cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
