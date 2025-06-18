836f8b3b11a1f5e5f33637c498713d3b010e8439
Baber Khan <bbrkhnn@outlook.com>
2025-06-18 17:26:22 +0100

    chore(deploy): configure production settings

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,12 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import path from "path";
 import registerRoutes from "./routes/index.js";
 import { globalErrorHandler } from "./utils/globalErrorHandler.js";
 import { app, httpServer } from "./utils/socket.js";
+
+const __dirname = path.resolve();
 
 app.use(
   cors({
@@ -18,5 +21,12 @@ app.use(cookieParser());
 registerRoutes(app);
 
 app.use(globalErrorHandler);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get("/{*splat}", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 export { httpServer };
