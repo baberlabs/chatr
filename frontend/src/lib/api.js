d3807d21a8d4fdaa2501eeaa3cd4172b70d476f7
Baber Khan <bbrkhnn@outlook.com>
2025-06-20 22:02:02 +0100

    fix(api): use correct api url in local development

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const api = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
-      ? "http://localhost:5001"
+      ? "http://localhost:5001/api/v1"
       : "/api/v1",
   withCredentials: true,
 });
